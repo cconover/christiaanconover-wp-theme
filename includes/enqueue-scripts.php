ee1e09e65adedd525aec38921fb8f5232c50e550
Christiaan Conover <dev@cconover.com>
2015-08-06 01:14:16 -0400

    Switch to self-hosted jQuery library.

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -12,62 +12,59 @@
 
 if ( ! function_exists( 'christiaanconover_scripts' ) ) :
 	function christiaanconover_scripts() {
+		// Enqueue the main Stylesheet.
+		wp_register_style(
+			THEME_ID,
+			get_stylesheet_directory_uri() . '/assets/css/' . THEME_ID . '.css',
+			array(),
+			THEME_VERSION
+		);
 
-	// Enqueue the main Stylesheet.
-	wp_register_style(
-		THEME_ID,
-		get_stylesheet_directory_uri() . '/assets/css/' . THEME_ID . '.css',
-		array(),
-		THEME_VERSION
-	);
+		// Deregister the jquery version bundled with WordPress.
+		wp_deregister_script( 'jquery' );
 
-	// Deregister the jquery version bundled with WordPress.
-	wp_deregister_script( 'jquery' );
+		// Register our own copy of jQuery.
+		wp_enqueue_script(
+			'jquery',
+			get_template_directory_uri() . '/assets/js/lib/jquery.js',
+			array(),
+			THEME_VERSION,
+			false
+		);
 
-	// Modernizr is used for polyfills and feature detection. Must be placed in header.
-	wp_register_script(
-		'modernizr',
-		get_template_directory_uri() . '/assets/js/lib/modernizr.js',
-		array(),
-		THEME_VERSION,
-		false
-	);
+		// Modernizr is used for polyfills and feature detection. Must be placed in header.
+		wp_register_script(
+			'modernizr',
+			get_template_directory_uri() . '/assets/js/lib/modernizr.js',
+			array(),
+			THEME_VERSION,
+			false
+		);
 
-	// Fastclick removes the 300ms delay on click events in mobile environments. Must be placed in header.
-	wp_register_script(
-		'fastclick',
-		get_template_directory_uri() . '/assets/js/lib/fastclick.js',
-		array(),
-		THEME_VERSION,
-		false
-	);
+		// Fastclick removes the 300ms delay on click events in mobile environments. Must be placed in header.
+		wp_register_script(
+			'fastclick',
+			get_template_directory_uri() . '/assets/js/lib/fastclick.js',
+			array(),
+			THEME_VERSION,
+			false
+		);
 
-	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-	wp_enqueue_script(
-		'jquery',
-		'//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js',
-		array(),
-		THEME_VERSION,
-		false
-	);
+		// Concatenated theme JavaScript.
+		wp_register_script(
+			THEME_ID,
+			get_template_directory_uri() . '/assets/js/' . THEME_ID . '.js',
+			array( 'jquery', 'fastclick', 'modernizr' ),
+			THEME_VERSION,
+			true
+		);
 
-	// Concatenated theme JavaScript.
-	wp_register_script(
-		THEME_ID,
-		get_template_directory_uri() . '/assets/js/' . THEME_ID . '.js',
-		array( 'jquery', 'fastclick', 'modernizr' ),
-		THEME_VERSION,
-		true
-	);
+		// Enqueue stylesheets.
+		wp_enqueue_style( THEME_ID );
 
-	// Enqueue stylesheets.
-	wp_enqueue_style( THEME_ID );
-
-	// Enqueue all registered scripts.
-	wp_enqueue_script( THEME_ID );
-
+		// Enqueue all registered scripts.
+		wp_enqueue_script( THEME_ID );
 	}
-
 	add_action( 'wp_enqueue_scripts', 'christiaanconover_scripts' );
 endif;
 
